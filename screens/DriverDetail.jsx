5a33dac6a1e765df78ae49fc5d6921fadd28f664
albertoesco <alumno.538326@ies.azarquiel.es>
2024-03-09 11:45:28 +0100

    AjustesDetail

--- screens/DriverDetail.jsx
+++ screens/DriverDetail.jsx
@@ -4,35 +4,69 @@ import { View, Text, Image, StyleSheet } from 'react-native';
 export default function DriverDetail({ route }) {
     const { item } = route.params;
     return (
-        <View style={styles.container}>
-            <Image source={{ uri: item.headshot_url }} style={styles.image} resizeMode="contain" />
-            <View style={styles.details}>
-                <Text style={styles.boldText}>{item.full_name}</Text>
-                <Text style={styles.text}>{item.team_name}</Text>
-                <Text style={styles.text}>{item.country_code}</Text>
-                <Text style={styles.text}>{item.driver_number}</Text>
-                <Text style={styles.text}>{item.name_acronym}</Text>
-            </View>
+
+      
+      <View style={styles.column}>
+        <Image source={{ uri: item.headshot_url }} style={styles.image} resizeMode="contain" />
+        <View style={[styles.column]}>
+          <Text style={[styles.textCommon, { fontWeight: "bold" }]}>{item.full_name}</Text>
+
+          <Text style={[styles.textOfficial, { fontWeight: "bold" }]}>{item.name_acronym} {item.driver_number}</Text>
+
+          <Text style={[styles.text]}>
+            <Text style={styles.boldText}>• Country: </Text> 
+            {item.country_code}
+          </Text>
+
+          <Text style={[styles.text]}>
+            <Text style={styles.boldText}>• Team: </Text> 
+            {item.team_name}
+          </Text>
+
+          <Text style={[styles.text]}>
+            <Text style={styles.boldText}>• Number: </Text> 
+            {item.driver_number}
+          </Text>
+
         </View>
+      </View>
+  
     );
 }
 
 const styles = StyleSheet.create({
-    image: {
-      width: 300,
-      height: 300
-    },
-    row: {
-      flex: 1,
-      flexDirection: "row",
-      margin: 10
-    },
-    column: {
-      flex: 1,
-      flexDirection: "column",
-      margin: 20
-    },
-    text: {
-      fontSize: 18
-    }
-  });
+  image: {
+    width: 360,
+    height: 300,
+    marginLeft: 15
+  },
+  row: {
+    flex: 1,
+    flexDirection: "row",
+    margin: 10,
+  },
+  column: {
+    flex: 1,
+    flexDirection: "column",
+    margin: 10,
+  },
+  text: {
+    fontSize: 19,
+    marginBottom: 8,
+  },
+  textCommon: {
+    fontWeight: "bold",
+    fontSize: 24
+  },
+  textOfficial: { 
+    fontWeight: "bold", 
+    fontSize: 22, 
+    marginBottom: 10 
+  },
+  boldText: {
+    fontWeight: "bold",
+  },
+  ml: {
+    marginLeft: 12
+  }
+});
